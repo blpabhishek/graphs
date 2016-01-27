ef33d04305c04c4ddc7964f019420b1e96be5f13
Abhishek Gupta <abhishkg@thoughtworks.com>
2016-01-27 17:00:16 +0530

    Created library for weighted graph and implemented Dijkstra alogrithm to find the shortest path.

--- tests/weightedGraphsTest.js
+++ tests/weightedGraphsTest.js
@@ -25,7 +25,7 @@ describe("shortest path",function(){
 
 		var e1=new graphs.Edge("e1",'A','B',1);
 		var e2=new graphs.Edge("e2",'B','C',1);
-		var e3=new graphs.Edge("e1",'A','C',1);
+		var e3=new graphs.Edge("e3",'A','C',1);
 		g.addEdge(e1);
 		g.addEdge(e2);
 		g.addEdge(e3);
@@ -43,7 +43,7 @@ describe("shortest path",function(){
 
 		var e1=new graphs.Edge("e1",'A','B',1);
 		var e2=new graphs.Edge("e2",'B','C',1);
-		var e3=new graphs.Edge("e1",'A','C',3);
+		var e3=new graphs.Edge("e3",'A','C',3);
 		g.addEdge(e1);
 		g.addEdge(e2);
 		g.addEdge(e3);
@@ -68,5 +68,36 @@ describe("shortest path",function(){
 		assert.equal(1,path.length);
 		assert.deepEqual(e1,path[0]);
 	});
-
+	it("should works for dense graph", function() {
+        var densegraph = denseGraph();
+        var extra1 = new graphs.Edge('extra1','A','C',1);
+        var extra2 = new graphs.Edge('extra2','C','B',1);
+        densegraph.addEdge(extra1);
+        densegraph.addEdge(extra2);
+        
+        var path = densegraph.shortestPath('A', 'B');
+        assert.equal(2, path.length);
+        assert.deepEqual(extra1, path[0]);
+        assert.deepEqual(extra2, path[1]);
+    });
 });
+
+var denseGraph=function() {
+	var g=new graphs.WeightedGraph();
+	var vertices=['A','B','C','D','E','F','G','H','I','J','H'];
+
+	vertices.forEach(function(vertex){
+		g.addVertex(vertex);
+	});
+
+	for (var i = 0; i < vertices.length-1; i++) {
+		var from=vertices[i];
+		for (var j = i+1; j < vertices.length; j++) {
+			var first = new graphs.Edge('e'+i,from,vertices[j],5);
+			g.addEdge(first);
+			var second = new graphs.Edge('e'+i,vertices[j],from,5);
+			g.addEdge(second);
+		}
+	}
+	return g;
+}
